3cccb63411506608d49d94d1a0419495aae8c960
parnold-x <parnold-x@gmail.com>
2019-11-26 22:50:25 +0100

    remove strange character

--- meson/post_install.py
+++ meson/post_install.py
@@ -9,7 +9,7 @@ datadir = path.join(prefix, 'share')
 desktop_database_dir = path.join(datadir, 'applications')
 
 if not environ.get('DESTDIR'):
-    print('Compiling gsettings schemas…')
+    print('Compiling gsettings schemas')
     subprocess.call(['glib-compile-schemas', schemadir])
     print('Updating desktop database…')
     subprocess.call(['update-desktop-database', '-q', desktop_database_dir])
